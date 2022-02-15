b2febfba32500b696cb8aaad15ca940bb42f2b5a
inkonopka <ignacy.nkonopka@gmail.com>
2022-02-15 09:50:07 +0100

    spectate view of get-form

--- src/components/get-form/GetForm.js
+++ src/components/get-form/GetForm.js
@@ -25,18 +25,44 @@ export const GetForm =  () =>{
         Wybierz formularz
     </Heading>
     <Divider mt={12} mb={12} />
-    {datas && datas.map((data,index) => (
     <LinkBox as='article' maxW='sm' p='5' borderWidth='1px' rounded='md' mt={6} mb={6}>
         <Box as='time' dateTime='2021-01-15 15:30:00 +0000 UTC'>
-            Formularz nr: {index}
+            Formularz nr: 1
         </Box>
         <Heading size='md' my='2'>
             <LinkOverlay href='#'>
-            {data.name}
+            OBJAWY OGÓLNE - osłabienienie odporności: ciągle choruję: cały czas mi spływa wydzielina
             </LinkOverlay>
         </Heading>
     </LinkBox>
-    ))}
+    <LinkBox as='article' maxW='sm' p='5' borderWidth='1px' rounded='md' mt={6} mb={6}>
+        <Box as='time' dateTime='2021-01-15 15:30:00 +0000 UTC'>
+            Formularz nr: 2
+        </Box>
+        <Heading size='md' my='2'>
+            <LinkOverlay href='#'>
+            plecy - ból: schodzące do nogi: i mrowienie
+            </LinkOverlay>
+        </Heading>
+        {/* <Text>
+            Catch up on what’s been cookin’ at Smashing and explore some of the most
+            popular community resources.
+        </Text> */}
+    </LinkBox>
+    <LinkBox as='article' maxW='sm' p='5' borderWidth='1px' rounded='md' mt={6} mb={6}>
+        <Box as='time' dateTime='2021-01-15 15:30:00 +0000 UTC'>
+            Formularz nr: 3
+        </Box>
+        <Heading size='md' my='2'>
+            <LinkOverlay href='#'>
+            OBJAWY OGÓLNE - osłabienienie odporności: ciągle choruję: cały czas mi spływa wydzielina
+            </LinkOverlay>
+        </Heading>
+        {/* <Text>
+            Catch up on what’s been cookin’ at Smashing and explore some of the most
+            popular community resources.
+        </Text> */}
+    </LinkBox>
     <Center>
         <Button colorScheme='teal' size='lg'>
             Wylosuj inne
