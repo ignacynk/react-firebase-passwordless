833335bac5d88e1153b61eeeed7e8cb37c76bcf2
inkonopka <ignacy.nkonopka@gmail.com>
2022-02-15 11:39:11 +0100

    working get-form

--- src/components/get-form/GetForm.js
+++ src/components/get-form/GetForm.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
+import axios from "axios";
+import { useAuth } from '../../hooks/useAuth';
 // Import UI library 
 import { 
   GridItem, 
@@ -11,11 +13,38 @@ import {
   Divider,
   Center
 } from '@chakra-ui/react'
-import {datas} from "./data"
-// Import Components
-// import { AddButtonForm } from './AddButtonForm';
 
-export const GetForm =  () =>{ 
+
+export const GetForm =  () => { 
+    // Import user's email
+    const { user } = useAuth();
+
+
+    let [formsData, setFormsData] = useState([]);
+
+    // Import forms data at the begining
+    useEffect(() => {
+    axios
+        .get(
+        `https://medsi-api.ink2000.repl.co/api/getn/${user.email}`,{
+        
+        },
+        )
+        .then((response) => setFormsData(response.data));
+        
+    }, []);
+
+    // Function for button's onClick - kinda refresh data
+    const getNewOne = () => {
+        axios
+        .get(
+        `https://medsi-api.ink2000.repl.co/api/getn/${user.email}`,{
+        
+        },
+        )
+        .then((response) => setFormsData(response.data));
+    }
+
   return (
     <GridItem
     colStart={[1, null, null, 2, null, null]}
@@ -25,46 +54,20 @@ export const GetForm =  () =>{
         Wybierz formularz
     </Heading>
     <Divider mt={12} mb={12} />
-    <LinkBox as='article' maxW='sm' p='5' borderWidth='1px' rounded='md' mt={6} mb={6}>
+    {formsData && formsData.map((forms,i) => (
+    <LinkBox as='article' maxW='sm' p='5' borderWidth='1px' rounded='md' mt={6} mb={6} key={forms._id}>
         <Box as='time' dateTime='2021-01-15 15:30:00 +0000 UTC'>
-            Formularz nr: 1
+            Formularz nr: {i + 1}
         </Box>
         <Heading size='md' my='2'>
-            <LinkOverlay href='#'>
-            OBJAWY OGÓLNE - osłabienienie odporności: ciągle choruję: cały czas mi spływa wydzielina
+            <LinkOverlay href={`/form/${forms._id}`}>
+            {forms.name}
             </LinkOverlay>
         </Heading>
     </LinkBox>
-    <LinkBox as='article' maxW='sm' p='5' borderWidth='1px' rounded='md' mt={6} mb={6}>
-        <Box as='time' dateTime='2021-01-15 15:30:00 +0000 UTC'>
-            Formularz nr: 2
-        </Box>
-        <Heading size='md' my='2'>
-            <LinkOverlay href='#'>
-            plecy - ból: schodzące do nogi: i mrowienie
-            </LinkOverlay>
-        </Heading>
-        {/* <Text>
-            Catch up on what’s been cookin’ at Smashing and explore some of the most
-            popular community resources.
-        </Text> */}
-    </LinkBox>
-    <LinkBox as='article' maxW='sm' p='5' borderWidth='1px' rounded='md' mt={6} mb={6}>
-        <Box as='time' dateTime='2021-01-15 15:30:00 +0000 UTC'>
-            Formularz nr: 3
-        </Box>
-        <Heading size='md' my='2'>
-            <LinkOverlay href='#'>
-            OBJAWY OGÓLNE - osłabienienie odporności: ciągle choruję: cały czas mi spływa wydzielina
-            </LinkOverlay>
-        </Heading>
-        {/* <Text>
-            Catch up on what’s been cookin’ at Smashing and explore some of the most
-            popular community resources.
-        </Text> */}
-    </LinkBox>
+    ))}
     <Center>
-        <Button colorScheme='teal' size='lg'>
+        <Button colorScheme='teal' size='lg' onClick={getNewOne}>
             Wylosuj inne
         </Button>
     </Center>
